90cdcf729ed1c22e67fd11cbbcf8c066c9c54335
eyadmansour1 <145153318+eyadmansour1@users.noreply.github.com>
2025-11-02 17:20:35 -0400

    Update features.py

--- src/features.py
+++ src/features.py
@@ -1,7 +1,13 @@
+"""
+features.py
+Performs preprocessing and scaling for models.
+"""
+
 from sklearn.preprocessing import StandardScaler
 
-def scale_features(X_train, X_test):
+def scale_features(X_train, X_val, X_test):
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
+    X_val_scaled = scaler.transform(X_val)
     X_test_scaled = scaler.transform(X_test)
-    return X_train_scaled, X_test_scaled
+    return X_train_scaled, X_val_scaled, X_test_scaled, scaler
